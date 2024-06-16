f6266dd467a39346e005e5c85e2624060b6f8440
TecnoGuy77 <138028703+TecnoGuy77@users.noreply.github.com>
2024-06-16 16:46:32 +0530

    Day-2 Completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,10 +1,18 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+// ignore: unused_import
 import 'package:fluttermastery/utils/routes.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+String name = "";
+bool changeButton = false;
+
+class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -19,9 +27,9 @@ class LoginPage extends StatelessWidget {
               const SizedBox(
                 height: 5,
               ),
-              const Text(
-                "Welcome",
-                style: TextStyle(
+              Text(
+                "Welcome $name",
+                style: const TextStyle(
                     fontSize: 28,
                     fontWeight: FontWeight.bold,
                     color: Colors.deepPurpleAccent),
@@ -36,6 +44,11 @@ class LoginPage extends StatelessWidget {
                   children: [
                     //Username
                     TextFormField(
+                      onChanged: (value) {
+                        name = value;
+
+                        setState(() {});
+                      },
                       decoration: const InputDecoration(
                           hintText: "Enter Username", labelText: "Username"),
                     ),
@@ -51,21 +64,55 @@ class LoginPage extends StatelessWidget {
               ),
               const SizedBox(height: 25),
 
-              //Login Button
-              ElevatedButton(
-                style: TextButton.styleFrom(
-                  shape: LinearBorder.none,
-                  foregroundColor: Colors.white,
-                  backgroundColor: Colors.deepPurpleAccent,
-                  minimumSize: const Size(150, 40),
-                ),
-                onPressed: () {
-                  if (kDebugMode) {
-                    Navigator.popAndPushNamed(context, MyRoutes.homeRoute);
-                  }
+              InkWell(
+                focusColor: Colors.white,
+                splashColor: Colors.white,
+                autofocus: true,
+                onTap: () async {
+                  setState(() {
+                    changeButton = true;
+                  });
+                  await Future.delayed(const Duration(seconds: 1));
+                  // ignore: use_build_context_synchronously
+                  Navigator.pushNamed(context, MyRoutes.homeRoute);
                 },
-                child: const Text("Login"),
+                child: AnimatedContainer(
+                  duration: const Duration(seconds: 1),
+                  decoration: BoxDecoration(
+                    color: Colors.deepPurpleAccent,
+                    borderRadius: changeButton
+                        ? BorderRadius.circular(30)
+                        : BorderRadius.circular(8),
+                  ),
+                  width: changeButton ? 50 : 150,
+                  height: 50,
+                  alignment: Alignment.center,
+                  child: changeButton
+                      ? const Icon(
+                          Icons.done,
+                          color: Colors.white,
+                        )
+                      : const Text(
+                          "Login",
+                          style: TextStyle(color: Colors.white, fontSize: 18),
+                        ),
+                ),
               ),
+              //Login Button
+
+              // ElevatedButton(
+              //   style: TextButton.styleFrom(
+              //     shape: const RoundedRectangleBorder(
+              //         borderRadius: BorderRadius.all(Radius.circular(2.5))),
+              //     foregroundColor: Colors.white,
+              //     backgroundColor: Colors.deepPurpleAccent,
+              //     minimumSize: const Size(150, 40),
+              //   ),
+              //   onPressed: () {
+              //     Navigator.popAndPushNamed(context, MyRoutes.homeRoute);
+              //   },
+              //   child: const Text("Login"),
+              // ),
             ],
           ),
         ));
