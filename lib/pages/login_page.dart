eb41094dc01ec1e734c3f328a74e73f005f39ab6
TecnoGuy77 <138028703+TecnoGuy77@users.noreply.github.com>
2024-06-16 13:40:26 +0530

    Day-1 Completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttermastery/utils/routes.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
@@ -7,61 +9,65 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Material(
         color: Colors.white,
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(right: 70),
-              child: Image.asset(
-                height: 225,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Image.asset(
+                height: 250,
                 "assets/images/login_image.png",
               ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            const Text(
-              "Welcome",
-              style: TextStyle(
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.deepPurpleAccent),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
-              child: Column(
-                children: [
-                  //Username
-                  TextFormField(
-                    decoration: const InputDecoration(
-                        hintText: "Enter Username", labelText: "Username"),
-                  ),
-
-                  //Password
-                  TextFormField(
-                    obscureText: true,
-                    decoration: const InputDecoration(
-                        hintText: "Enter Password", labelText: "Password"),
-                  ),
-                ],
+              const SizedBox(
+                height: 5,
               ),
-            ),
-            const SizedBox(height: 20),
+              const Text(
+                "Welcome",
+                style: TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.deepPurpleAccent),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                    vertical: 16.0, horizontal: 32.0),
+                child: Column(
+                  children: [
+                    //Username
+                    TextFormField(
+                      decoration: const InputDecoration(
+                          hintText: "Enter Username", labelText: "Username"),
+                    ),
 
-            //Login Button
-            ElevatedButton(
-                style: const ButtonStyle(
-                    shape: WidgetStatePropertyAll(LinearBorder.none),
-                    backgroundColor:
-                        WidgetStatePropertyAll(Colors.deepPurpleAccent),
-                    foregroundColor: WidgetStatePropertyAll(Colors.white),
-                    overlayColor: WidgetStatePropertyAll(Colors.deepPurple)),
-                onPressed: () {},
-                child: const Text("Login"))
-          ],
+                    //Password
+                    TextFormField(
+                      obscureText: true,
+                      decoration: const InputDecoration(
+                          hintText: "Enter Password", labelText: "Password"),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 25),
+
+              //Login Button
+              ElevatedButton(
+                style: TextButton.styleFrom(
+                  shape: LinearBorder.none,
+                  foregroundColor: Colors.white,
+                  backgroundColor: Colors.deepPurpleAccent,
+                  minimumSize: const Size(150, 40),
+                ),
+                onPressed: () {
+                  if (kDebugMode) {
+                    Navigator.popAndPushNamed(context, MyRoutes.homeRoute);
+                  }
+                },
+                child: const Text("Login"),
+              ),
+            ],
+          ),
         ));
   }
 }
