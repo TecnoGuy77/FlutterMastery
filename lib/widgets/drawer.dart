44df481f79d028310206434b36ab5c2a0026195a
TecnoGuy77 <138028703+TecnoGuy77@users.noreply.github.com>
2024-06-18 17:26:49 +0530

    Day-4 Completed

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:fluttermastery/utils/routes.dart';
 
 class MyDrawer extends StatelessWidget {
   const MyDrawer({super.key});
